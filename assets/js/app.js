5633ee54e3731f85aec3521f6d62120d0bd699c7
Leslie A. Lopez <lesashley56@gmail.com>
2017-05-02 17:00:13 -0500

    agregando validaciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,67 @@ window.addEventListener("scroll", function () {
     header.classList.remove("header-color");
   }
 })
+//Visible inputs
+document.getElementById("visible").addEventListener("click",function () {
+  var inputHidden = document.getElementsByClassName("input-box");
+  inputHidden[1].style.display="block";
+  inputHidden[2].style.display="block";
+  inputHidden[3].style.display="block";
+})
+
+//Validacion
+var letras = function (e) {
+    var codLetra = e.keyCode;
+    if ((codLetra>=97 && codLetra<=122) || (codLetra>=65 && codLetra<=90) || codLetra == 39 || codLetra == 32 || codLetra==241 || codLetra==209) {
+      return true;
+    }
+    else {
+      return false;
+    }
+}
+var numeros = function(e) {
+    var codNumero = e.keyCode;
+    if ((codNumero>=48 && codNumero<=57)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+}
+
+var inputText = document.getElementsByClassName("input");
+inputText[0].onkeypress=numeros;
+inputText[1].onkeypress=letras;
+inputText[3].onkeypress=letras;
+
+var error = document.getElementsByClassName("error");
+inputText[0].addEventListener("blur", function () {
+  if (!/9[0-9]{8}/.test(inputText[0].value)) {
+    error[0].style.visibility="visible";
+  }
+  else{
+    error[0].classList.remove("icon-visible");
+  }
+});
+
+inputText[1].addEventListener("blur",function () {
+  if (!/([A-Z]{1}[a-zñáéíóú]\D+)$/.test(inputText[1].value)) {
+      console.log("Datos incorrectos");
+  }
+  else{
+
+  }
+})
+inputText[2].addEventListener("blur", function () {
+  if (!/\S+@\S+\.\S+/.test(inputText[2].value)) {
+    console.log("datos incorrectos");
+  }
+  else{
+
+  }
+})
+inputText[3].addEventListener("blur",function () {
+  if (!/([A-Z]{1}[a-zñáéíóú]\D+)$/.test(inputText[3].value)) {
+      console.log("Datos incorrectos");
+  }
+})
